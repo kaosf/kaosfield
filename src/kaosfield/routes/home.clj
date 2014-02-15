2b95f7b840271b5c59c757619a6bb6560bdf29e1
ka <ka.kaosf@gmail.com>
2014-02-15 23:53:33 +0900

    initialize
    execute "lein new luminus kaosfield"
    
    Leiningen version is 2.3.4

---
+++ src/kaosfield/routes/home.clj
@@ -0,0 +1,15 @@
+(ns kaosfield.routes.home
+  (:use compojure.core)
+  (:require [kaosfield.views.layout :as layout]
+            [kaosfield.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
