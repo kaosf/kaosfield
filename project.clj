ace781dc3b0a8c21a9295524801efc524fd747e3
ka <ka.kaosf@gmail.com>
2014-03-12 01:48:30 +0900

    Try to use Clojure 1.6.0-RC1
    ref.
    * https://groups.google.com/forum/#!topic/clojure/zcjcFrMYWzc
    * https://github.com/clojure/clojure/blob/master/changes.md

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject kaosfield "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0-RC1"]
                  [lib-noir "0.8.0"]
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
