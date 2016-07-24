daac834c950bb1907a2676949534910904d4a278
ka <ka.kaosf@gmail.com>
2016-07-25 02:35:06 +0900

    Fix "target-dir" and function name from "deploy" to "copy-resources"

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-mkdir -p target
-cd target
+mkdir -p target/site
+cd target/site
 npm install crypto-js@3.1.6
--- src/net/kaosfield/www/core.clj
+++ src/net/kaosfield/www/core.clj
@@ -9,17 +9,17 @@
 (def pages {"/index.html" (jade/render "index.jade")
             "/log/index.html" (jade/render "log/index.jade")})
 
-(def target-dir "target/")
+(def target-dir "target/site")
 
-(defn- deploy []
-  (shell/sh "mkdir" "target")
+(defn- copy-resources []
+  (shell/sh "mkdir" "-p" target-dir)
   (map
-    #(shell/sh "cp" "-r" (str "resources/public/" %1) "target")
+    #(shell/sh "cp" "-r" (str "resources/public/" %1) target-dir)
     ["css" "fonts" "img" "js" "log" "stylesheets" "tgk"]))
 
 (defn -main
-  "Generate static pages and deploy them."
+  "Generate static pages and copy resources (assets)"
   []
   (stasis/empty-directory! target-dir)
   (stasis/export-pages pages target-dir)
-  (deploy))
+  (copy-resources))
