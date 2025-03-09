a6be4b90450399b30a00920d951f87278f9a1934
ka <ka.kaosf@gmail.com>
2025-03-09 17:23:20 +0900

    Update checksum on build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 npx pug3 src --out static
 npx css-minify -f css/style.css -o . && mv style.min.css static/stylesheets/style.css
+sha256sum static/icon.webp static/index.html static/qr.png static/stylesheets/style.css > checksum.sha256
